bfe0e268f705281fe3c5a8b3b5dbbf4eeeca5323
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-10-21 13:52:59 -0300

    add swagger module

--- nestjs-mail-schedule/src/main.ts
+++ nestjs-mail-schedule/src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Mail Schedule API')
+    .setDescription('Mail scheduler')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/document', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
