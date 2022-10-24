951c32669c726b22d40012cf873504f8608b81d0
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-10-23 23:18:09 -0300

    enable CORS

--- nestjs-mail-schedule/src/main.ts
+++ nestjs-mail-schedule/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
